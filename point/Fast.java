dbc4cb905b3518d17e96d765e530948a39d381f5
Hao Tan <ihntee@gmail.com>
2014-07-09 19:26:19 +0800

    finish Fast.java for upload

--- point/Fast.java
+++ point/Fast.java
@@ -9,43 +9,79 @@ public class Fast {
         StdOut.println(points[end - 1]);
     }
 
+    // check if a point is the least element in a set of
+    // fragments of a given slope
+    private static boolean isLeast(Point p, Point[] points, double slope) {
+        final int n = points.length;
+
+        // copy the points array
+        Point[] temp = new Point[n];
+        for (int i = 0; i < n; i++) {
+            temp[i] = points[i];
+        }
+
+        // 1- For any virtual origin, consider all points.
+        // 2- For any segment, check if the origin is the least element.
+        Arrays.sort(temp, 0, n, p.SLOPE_ORDER);
+
+        for (int i = 1; i < n; i++) {
+            if (p.slopeTo(temp[i]) == slope) {
+                if (p.compareTo(temp[i]) > 0)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
         final int N = StdIn.readInt();
         Point[] points = new Point[N];
+
+        // an auxiliary array
+        Point[] aux = new Point[N];
 
         // read the points
         for (int i = 0; i < N; i++) {
             int x = StdIn.readInt();
             int y = StdIn.readInt();
             points[i] = new Point(x, y);
+            aux[i] = new Point(x, y);
         }
 
         // sort the points
         Arrays.sort(points, 0, N);
+        Arrays.sort(aux, 0, N);
 
         for (int i = 0; i < N - 3; i++) {
-
             // sort in slope
             Arrays.sort(points, i + 1, N, points[i].SLOPE_ORDER);
-
+            
             // find a list of colinear points and print
             int count = 1, start = i + 1;
-            for (int j = i + 2; j < N; j++) {
+            for (int j = 1; j < N; j++) {
                 if (points[i].slopeTo(points[j]) == points[i].slopeTo(points[j - 1])){
                     count++;
                 } else {
-                    if (count >= 3)
+                    if (count >= 3 && isLeast(points[i], points, points[i].slopeTo(points[j - 1]))) {
                         print(points[i], points, start, start + count);
+                    }
                     start = j;
                     count = 1;
                 }
 
-                if (j == N - 1 && count >= 3)
+                if (j == N - 1 && count >= 3 && isLeast(points[i], points, points[i].slopeTo(points[j - 1])))
                     print(points[i], points, start, start + count);
-
             }
+            
+
+            // copy aux back to points
+            for (int q = 0; q < points.length; q++)
+                points[q] = aux[q];
+
             /*
             // print the points
+            StdOut.println(points[i] + " : ");
             for (int j = i + 1; j < N; j++) {
                 StdOut.print(points[j] + " -> ");
 
